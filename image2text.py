7ff3aeea0b547dcd0d0ba5012f22df3c37a447aa
st14d07 <st14d07@gm.ibaraki-ct.ac.jp>
2019-01-22 05:23:53 +0900

    douda??

--- image2text.py
+++ image2text.py
@@ -1,4 +1,4 @@
-""" Tesseract OCR Test Program within MPI """
+""" Tesseract OCR Test Program with MPI """
 # coding: UTF-8
 
 import sys
@@ -17,30 +17,27 @@ PDF_DIR = 'pdf/'
 IMG_DIR = 'pdf2img/' + PDF_NAME + '/'
 IMG_TYPE = '.png'
 TXT_DIR = 'txt/'
-# MasterNode's Proc No.
+BENCHMARK_LOC = 'bm/test.png'
+
 MASTER_NODE = 0
 
-comm = MPI.COMM_WORLD
-size = comm.Get_size()
-rank = comm.Get_rank()
-proc_name = MPI.Get_processor_name()
-print("Running from processor %s, rank %d out of %d processors." % (proc_name, rank, size))
-comm.Barrier()
+
+def create_2dlist(num):
+    array = []
+    for _ in range(num):
+        array.append([])
+    return array
 
 
 def ocr_tool_select():
-    # 利用できるOCRエンジン
     tools = pyocr.get_available_tools()
     if not tools:
         print('[{}{}]No OCR tool found.'.format(rank, proc_name))
         sys.exit(1)
-    # Tesseract OCRを選択
     my_tool = tools[0]
     # print('[{}]Will use tool {}'.format(rk, my_tool.get_name()))
-    # 利用できる言語
     # langs = my_tool.get_available_languages()
     # print("Available languages: %s" % ", ".join(langs))
-    # 日本語を選択
     my_lang = 'jpn'
     # print("[{}]Will use lang {}".format(rk, my_lang))
     return my_tool, my_lang
@@ -53,7 +50,7 @@ def image_analyze(jn, ocr_cfg):
     raw_text = ocr_cfg[0].image_to_string(
         Image.open(path),
         lang=ocr_cfg[1],
-        builder=pyocr.builders.TextBuilder()
+        builder=pyocr.builders.TextBuilder(tesseract_layout=6)
     )
     raw_text2 = regex.sub(r'((\s)*((\p{Han})|(\p{Hiragana})|(\p{Katakana})|[、。？！])|(\n){2})', r'\3', raw_text)
     text = regex.sub(r'\n|([ ]+)', r' ', raw_text2)
@@ -64,17 +61,37 @@ def image_analyze(jn, ocr_cfg):
     return text, analyze_time
 
 
-def create_2dlist(num):
-    array = []
-    for _ in range(num):
-        array.append([])
-    return array
+def benchmark(ocr_cfg):
+    bm_start = MPI.Wtime()
+    ocr_cfg[0].image_to_string(
+        Image.open(BENCHMARK_LOC),
+        lang=ocr_cfg[1],
+        builder=pyocr.builders.TextBuilder(tesseract_layout=6)
+    )
+    bm_time = MPI.Wtime() - bm_start
+    return bm_time
 
+
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+proc_name = MPI.Get_processor_name()
+print("Running from processor %s, rank %d out of %d processors." % (proc_name, rank, size))
+comm.Barrier()
 
 if rank == MASTER_NODE:
     whole_start = MPI.Wtime()
+
+ocr_config = ocr_tool_select()
+bm_result = benchmark(ocr_config)
+bm_results = comm.gather(bm_result, root=0)
+
+comm.Barrier()
+
+if rank == MASTER_NODE:
     PAGE_NUM = 0
 
+    print(bm_results)
     if not os.path.exists(IMG_DIR):
         os.mkdir(IMG_DIR)
     pdf_images = convert_from_path('{0}{1}.pdf'.format(PDF_DIR, PDF_NAME))
@@ -83,7 +100,7 @@ if rank == MASTER_NODE:
         PAGE_NUM += 1
 
     file_data = []
-    print('Number of Pages: ' + str(PAGE_NUM))
+    # print('Number of Pages: ' + str(PAGE_NUM))
 
     # get size of files and store them
     for i in range(PAGE_NUM):
@@ -96,28 +113,29 @@ if rank == MASTER_NODE:
     # sizes of images
     worker_size = [0] * size
 
+    for i in range(rank):
+        bm_results[i] /= bm_results[0]
+
     # job equalizing
-    for i in range(0, PAGE_NUM):
+    for i in range(PAGE_NUM):
         min_size_index = worker_size.index(min(worker_size))
         jobs[min_size_index].append(file_data_s[i])
-        worker_size[min_size_index] += file_data_s[i].get("size")
-    for i in range(0, size):
-        print("[{}]{}\tlength: {}({})".format(i, jobs[i], len(jobs[i]), worker_size))
+        worker_size[min_size_index] += file_data_s[i].get("size") * bm_results[min_size_index]
+    # for i in range(size):
+    #    print("[{}]{}\tlength: {}({})".format(i, jobs[i], len(jobs[i]), worker_size[i]))
 
     req = create_2dlist(size)
     # Send workers
     for i in range(1, size):
         # print('Now sending to ' + str(i))
         comm.send(len(jobs[i]), dest=i, tag=i)
-        print('jobs[{}]: {}'.format(i, jobs[i]))
-        for j in range(0, len(jobs[i])):
+        # print('jobs[{}]: {}'.format(i, jobs[i]))
+        for j in range(len(jobs[i])):
             msg = jobs[i][j]['name']
             req[i].append(comm.isend(msg, dest=i, tag=i))
             # print('MSG to {}: {}, {}'.format(i, msg, req[i][j]))
             req[i][j].wait()
-        print('[%d]All Sent to No.%d!' % rank, i)
-
-    proc_start = MPI.Wtime()
+        # print('[%d]All Sent to No.%d!' % (rank, i))
 
     my_jobs = []
     my_num = len(jobs[0])
@@ -127,6 +145,7 @@ if rank == MASTER_NODE:
 
     ocr_config = ocr_tool_select()
     master_result = []
+    proc_start = MPI.Wtime()
     for job_name in my_jobs:
         analyze_res = image_analyze(job_name, ocr_config)
         master_result.append({
@@ -138,15 +157,15 @@ if rank == MASTER_NODE:
 
     # Receive from workers
     for i in range(1, size):
-        print("Now receiving from {}".format(i))
+        # print("Now receiving from {}".format(i))
         req = comm.irecv(source=i, tag=0)
         recv_result = req.wait()
-        print("[0]Received!!".format(i))
-        print('[{}]JOB_NUM: {}'.format(i, len(recv_result)))
+        # print("[0]Received!!".format(i))
+        # print('[{}]JOB_NUM: {}'.format(i, len(recv_result)))
         for j in range(len(recv_result)):
             master_result.append(recv_result[j])
 
-    print('All received!')
+    # print('All received!')
     result_s = sorted(master_result, key=itemgetter('name'))
 
     f = open(TXT_DIR + PDF_NAME + '.txt', 'w')
@@ -161,27 +180,26 @@ if rank == MASTER_NODE:
 
     f.close()
     f_csv.close()
-    print('Writing Complete!\nLocation:{}'.format(TXT_DIR))
+    # print('Writing Complete!\nLocation:{}'.format(TXT_DIR))
     print(u"Congratulations!: %5.4fs." % (MPI.Wtime() - whole_start))
 
 if rank != MASTER_NODE:
-    proc_start = MPI.Wtime()
-
     my_jobs = []
     req = comm.irecv(source=0, tag=rank)
     my_num = req.wait()
     req = []
-    print('[{}]N of job: {}\nReceiving...'.format(rank, my_num))
+    print('[{}] of job: {}\nReceiving...'.format(rank, my_num))
     tmp_res = []
     for i in range(0, my_num):
         req.append(comm.irecv(source=0, tag=rank))
         my_jobs.append(req[i].wait())
-        print(my_jobs)
-    print('[{}]Received!'.format(rank))
+        # print(my_jobs)
+    # print('[{}]Received!'.format(rank))
     # print(my_jobs)
 
     ocr_config = ocr_tool_select()
     my_result = []
+    proc_start = MPI.Wtime()
     for job_name in my_jobs:
         analyze_res = image_analyze(job_name, ocr_config)
         my_result.append({
@@ -189,8 +207,8 @@ if rank != MASTER_NODE:
             'text': analyze_res[0],
             'time': analyze_res[1]
         })
-    print('[%d]Tasks completed. Now sending...' % rank)
+    print('[%d]Tasks completed!: %5.4fs.\nNow sending...' % (rank, MPI.Wtime() - proc_start))
 
     req = comm.isend(my_result, dest=0, tag=0)
     req.wait()
-    print('[%d]Sending completed!: %5.4fs.' % (rank, (MPI.Wtime() - proc_start)))
+    # print('[%d]Sending completed!' % rank)
--- image2text_a.py
+++ image2text_a.py
@@ -1,124 +1,205 @@
-""" Tesseract OCR Test Program within MPI """
+""" Tesseract OCR Test Program with MPI """
+# 画像サイズによるジョブ最適化を行わない場合
 # coding: UTF-8
 
 import sys
-import io
+import os
+from operator import itemgetter
 from PIL import Image
 from mpi4py import MPI
 from pdf2image import convert_from_path
 import regex
 import pyocr
 import pyocr.builders
+import csv
 
-#sys.stdin  = io.TextIOWrapper(sys.stdin.buffer,  encoding='utf-8')
-#sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
-#sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
+PDF_NAME = str(sys.argv[1])
+PDF_DIR = 'pdf/'
+IMG_DIR = 'pdf2img/' + PDF_NAME + '/'
+IMG_TYPE = '.png'
+TXT_DIR = 'txt/'
+BENCHMARK_LOC = 'bm/test.png'
 
-fn = str(sys.argv[1])
+MASTER_NODE = 0
 
-MasterNode = 0
+
+def create_2dlist(num):
+    array = []
+    for _ in range(num):
+        array.append([])
+    return array
+
+
+def ocr_tool_select():
+    tools = pyocr.get_available_tools()
+    if not tools:
+        print('[{}{}]No OCR tool found.'.format(rank, proc_name))
+        sys.exit(1)
+    my_tool = tools[0]
+    # print('[{}]Will use tool {}'.format(rk, my_tool.get_name()))
+    # langs = my_tool.get_available_languages()
+    # print("Available languages: %s" % ", ".join(langs))
+    my_lang = 'jpn'
+    # print("[{}]Will use lang {}".format(rk, my_lang))
+    return my_tool, my_lang
+
+
+def image_analyze(jn, ocr_cfg):
+    analyze_start = MPI.Wtime()
+    path = IMG_DIR + str(jn) + IMG_TYPE
+    print('[{}]Analyzing {}...'.format(rank, path))
+    raw_text = ocr_cfg[0].image_to_string(
+        Image.open(path),
+        lang=ocr_cfg[1],
+        builder=pyocr.builders.TextBuilder(tesseract_layout=6)
+    )
+    raw_text2 = regex.sub(r'((\s)*((\p{Han})|(\p{Hiragana})|(\p{Katakana})|[、。？！])|(\n){2})', r'\3', raw_text)
+    text = regex.sub(r'\n|([ ]+)', r' ', raw_text2)
+    # print("[{}]Analyzing finished!: {}".format(rk, text))
+    analyze_time = MPI.Wtime() - analyze_start
+    print("[{}]Analyzing finished!".format(rank))
+    print("Time[%s]: %4.5fs." % (jn, analyze_time))
+    return text, analyze_time
+
+
+def benchmark(ocr_cfg):
+    bm_start = MPI.Wtime()
+    ocr_cfg[0].image_to_string(
+        Image.open(BENCHMARK_LOC),
+        lang=ocr_cfg[1],
+        builder=pyocr.builders.TextBuilder(tesseract_layout=6)
+    )
+    bm_time = MPI.Wtime() - bm_start
+    return bm_time
+
 
 comm = MPI.COMM_WORLD
-
 size = comm.Get_size()
 rank = comm.Get_rank()
-procName = MPI.Get_processor_name()
-
-print("Running from processor %s, rank %d out of %d processors." % (procName, rank, size))
+proc_name = MPI.Get_processor_name()
+print("Running from processor %s, rank %d out of %d processors." % (proc_name, rank, size))
 
 comm.Barrier()
 
-if rank == MasterNode:
+if rank == MASTER_NODE:
     whole_start = MPI.Wtime()
-    """
-    images = convert_from_path('pdf/{}.pdf'.format(fn))
-    i = 0
-    for image in images:
-        image.save('pdf2img/{0}{1}.png' . format(fn, i+1), 'png')
-        i += 1
-    #画像の枚数をnimgに格納
-    nimg = i
-    """
-    nimg = 2
-    req = []
-    for j in range(1, size):
-        if j > nimg:
-            print('Now sending empty message to {}'.format(j))
-            req.append(comm.isend('0', dest=j, tag=j))
+    PAGE_NUM = 0
 
-        else:
-            print('Now sending {0}{1}.png to {2}'.format(fn, j, j))
-            req.append(comm.isend('pdf2img/{0}{1}.png'.format(fn, j), dest=j, tag=j))
-        req[j-1].wait()
+    if not os.path.exists(IMG_DIR):
+        os.mkdir(IMG_DIR)
+    pdf_images = convert_from_path('{0}{1}.pdf'.format(PDF_DIR, PDF_NAME))
+    for image in pdf_images:
+        image.save('{0}{1}{2}{3}'.format(IMG_DIR, PDF_NAME, PAGE_NUM, IMG_TYPE))
+        PAGE_NUM += 1
 
-comm.Barrier()
+    file_data = []
+    # print('Number of Pages: ' + str(PAGE_NUM))
 
-if rank != MasterNode:
-    proc_start = MPI.Wtime()
-    rreq = comm.irecv(source=0, tag=rank)
-    data = rreq.wait()
-    if data == '0':
-        print('Now sending 0 to master from {}'.format(rank))
-        """
-        sreq = comm.isend('0', dest=0, tag=rank)
-        sreq.wait()
-        """
-        comm.send('0', dest=0, tag=rank)
-    else:
-        print('Now Analyzing {} from {}'.format(data, rank))
-        
-        # 利用できるOCRエンジン
-        tools = pyocr.get_available_tools()
-        if not tools:
-            print("No OCR tool found")
-            sys.exit(1)
+    # get size of files and store them
+    for i in range(PAGE_NUM):
+        file_size = os.path.getsize('{}{}{}{}'.format(IMG_DIR, PDF_NAME, i, IMG_TYPE))
+        file_data.append({'name': PDF_NAME + str(i), 'size': round(file_size, 2)})
+    # sort by filesize ascending
+    # file_data_s = sorted(file_data, key=itemgetter('size'), reverse=True)
 
-        # Tesseract OCRを選択
-        tool = tools[0]
-        print("Will use tool '%s'" % (tool.get_name()))
+    jobs = create_2dlist(size)
+    # sizes of images
+    worker_size = [0] * size
 
-        # 利用できる言語
-        langs = tool.get_available_languages()
-        print("Available languages: %s" % ", ".join(langs))
+    # for i in range(rank):
+    #    bm_results[i] /= bm_results[0]
 
-        # 日本語を選択
-        lang = "jpn"
-        print("Will use lang '%s'" % (lang))
+    # job equalizing
+    for i in range(PAGE_NUM):
+        jobs[i % size].append(file_data[i])
 
-        txt = tool.image_to_string(
-            Image.open(data),
-            lang=lang,
-            builder=pyocr.builders.TextBuilder()
-        )
-        print('Now sending to master from {}'.format(rank))
-        """
-        sreq = comm.isend(txt, dest=0, tag=rank)
-        sreq.wait()
-        """
-        comm.send(txt, dest=0, tag=rank)
-
-    print('Sending Complete from {}'.format(rank))
-    print("プロセス%d実行時間: %5.4fs" % (rank, MPI.Wtime() - proc_start))
-    MPI.Finalize()
-
-if rank == MasterNode:
-    text = ''
+    req = create_2dlist(size)
+    # Send workers
     for i in range(1, size):
-        print("Now Received from {}".format(i))
-        req = comm.irecv(source=i, tag=i)
-        data = req.wait()
-        if data != '0':
-            data = regex.sub(r'((\s)*((\p{Han})|(\p{Hiragana})|(\p{Katakana})|[、。？！])|(\n){2})', r'\3', data)
-            data = regex.sub(r'\n|([ ]+)', r' ', data)
-            if text == '':
-                text = data
-            else:
-                text += '\n' + data
-            print(text + ' is appended.')
+        # print('Now sending to ' + str(i))
+        comm.send(len(jobs[i]), dest=i, tag=i)
+        # print('jobs[{}]: {}'.format(i, jobs[i]))
+        for j in range(0, len(jobs[i])):
+            msg = jobs[i][j]['name']
+            req[i].append(comm.isend(msg, dest=i, tag=i))
+            # print('MSG to {}: {}, {}'.format(i, msg, req[i][j]))
+            req[i][j].wait()
+        # print('[%d]All Sent to No.%d!' % (rank, i))
 
-    print(u"--- 結果 ---\n" + text)
+    my_jobs = []
+    my_num = len(jobs[0])
+    for i in range(0, my_num):
+        my_jobs.append(jobs[0][i]['name'])
+    print('Master,s job: {}'.format(my_jobs))
 
-    f = open('txt/' + fn + '.txt', 'w')
-    text.encode('utf-8')
-    f.write(text)
-    print(u"全実行時間: %5.4fs." % (MPI.Wtime() - whole_start))
+    ocr_config = ocr_tool_select()
+    master_result = []
+    proc_start = MPI.Wtime()
+    for job_name in my_jobs:
+        analyze_res = image_analyze(job_name, ocr_config)
+        master_result.append({
+            'name': int(job_name.replace(PDF_NAME, '')),
+            'text': analyze_res[0],
+            'time': analyze_res[1]
+        })
+    print('[0]Task completed!: %5.4fs' % (MPI.Wtime() - proc_start))
+
+    # Receive from workers
+    for i in range(1, size):
+        # print("Now receiving from {}".format(i))
+        req = comm.irecv(source=i, tag=0)
+        recv_result = req.wait()
+        # print("[0]Received!!".format(i))
+        # print('[{}]JOB_NUM: {}'.format(i, len(recv_result)))
+        for j in range(len(recv_result)):
+            master_result.append(recv_result[j])
+
+    # print('All received!')
+    result_s = sorted(master_result, key=itemgetter('name'))
+
+    f = open(TXT_DIR + PDF_NAME + '.txt', 'w')
+    f_csv = open(TXT_DIR + PDF_NAME + '.csv', 'w')
+    writer = csv.writer(f_csv, lineterminator='\n')
+    writer.writerow(['name', 'size[kB]', 'time[s]'])
+    for i in range(PAGE_NUM):
+        result_s[i]['size'] = file_data[i]['size']
+        result_s[i]['text'].encode('utf-8')
+        f.write('{}{}{}:\n\n{}\n\n\n'.format(PDF_NAME, i, IMG_TYPE, result_s[i]['text']))
+        writer.writerow([PDF_NAME + str(result_s[i]['name']), float(result_s[i]['size']/1000), result_s[i]['time']])
+
+    f.close()
+    f_csv.close()
+    # print('Writing Complete!\nLocation:{}'.format(TXT_DIR))
+    print(u"Congratulations!: %5.4fs." % (MPI.Wtime() - whole_start))
+
+if rank != MASTER_NODE:
+
+    my_jobs = []
+    req = comm.irecv(source=0, tag=rank)
+    my_num = req.wait()
+    req = []
+    print('[{}],s Job: {}\nReceiving...'.format(rank, my_num))
+    tmp_res = []
+
+    for i in range(0, my_num):
+        req.append(comm.irecv(source=0, tag=rank))
+        my_jobs.append(req[i].wait())
+        # print(my_jobs)
+    # print('[{}]Received!'.format(rank))
+    # print(my_jobs)
+
+    ocr_config = ocr_tool_select()
+    my_result = []
+    proc_start = MPI.Wtime()
+    for job_name in my_jobs:
+        analyze_res = image_analyze(job_name, ocr_config)
+        my_result.append({
+            'name': int(job_name.replace(PDF_NAME, '')),
+            'text': analyze_res[0],
+            'time': analyze_res[1]
+        })
+    print('[%d]Tasks completed!: %5.4fs.\nNow sending...' % (rank, MPI.Wtime() - proc_start))
+
+    req = comm.isend(my_result, dest=0, tag=0)
+    req.wait()
+    # print('[%d]Sending completed!' % rank)
